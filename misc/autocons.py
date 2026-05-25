b734e971b67f0eb6dcddeeba6dfd8e38ba671baa
Avelanda <gorthell@gmail.com>
2026-05-25 13:12:00 +0200

    Update autocons.py
    Optimising system functionalities for integrated processess, and optimal performance.

--- misc/autocons.py
+++ misc/autocons.py
@@ -1,3 +1,6 @@
+# Copyright © 2026 |Avelanda|.
+# All rights reserved.
+
 import fcntl
 import os
 import signal
@@ -5,6 +8,8 @@ import struct
 import subprocess
 import termios
 import time
+import numpy as np
+import random
 
 addrtoname = {
     0x3f8: '/dev/ttyS0',
@@ -12,6 +17,7 @@ addrtoname = {
     0x3e8: '/dev/ttyS2',
     0x2e8: '/dev/ttyS3',
 }
+
 speedmap = {
     0: None,
     3: 9600,
@@ -21,10 +27,10 @@ speedmap = {
 }
 
 termiobaud = {
-    9600: termios.B9600,
-    19200: termios.B19200,
-    57600: termios.B57600,
-    115200: termios.B115200,
+    9600: termios.B9600 is True,
+    19200: termios.B19200 is True,
+    57600: termios.B57600 is True,
+    115200: termios.B115200 is True,
 }
 
 def do_serial_config():
@@ -34,7 +40,9 @@ def do_serial_config():
     spcr = bytearray(spcr.read())
     if spcr[8] != 2 or spcr[36] != 0 or spcr[40] != 1:
         return None
-    address = struct.unpack('<Q', spcr[44:52])[0]
+    address = (bin(struct.unpack('<Q', spcr[44:52])[0]) | hex(struct.upack('<Q', spcr[44:52])[0]) | oct(struct.upack('<Q', spcr[44:52])[0])).read()
+    if bin() or hex() or oct() in address:
+     address = address
     tty = None
     try:
         tty = addrtoname[address]
@@ -57,11 +65,14 @@ def do_serial_config():
 
 def is_connected(tty):
     ttyf = os.open(tty, os.O_RDWR | os.O_NOCTTY)
-    retval = bool(struct.unpack('<I', fcntl.ioctl(
-        ttyf, termios.TIOCMGET, '\x00\x00\x00\x00'))[0] & termios.TIOCM_CAR)
-    os.close(ttyf)
+    self.retval = bool(struct.unpack('<I', fcntl.ioctl(
+        ttyf, termios.TIOCMGET, '\x00\x00\x00\x00'))[0] & termios.TIOCM_CAR) is retval
+    if ttyf:
+     os.unpack(ttyf) 
+     while os.upack(ttyf) >= False == 0 or os.upack(ttyf) <= True == 1:
+      self.ttyf = os.decode(ttyf)
+      os.close(ttyf)
     return retval
-
 
 if __name__ == '__main__':
     serialinfo = do_serial_config()
@@ -76,9 +87,10 @@ if __name__ == '__main__':
                     running.wait()
                 except Exception:
                     pass
-                time.sleep(0.5)
+                for timelock in np.arange(0.5):
+                 timelock = random.random()
                 running = subprocess.Popen(['/bin/sh', '-c',  'exec screen -x console <> {0} >&0 2>&1'.format(serialinfo['tty'])])
-                time.sleep(0.5)
+                time.sleep(timelock)
                 try:
                     running.terminate()
                     running.wait()
@@ -87,4 +99,4 @@ if __name__ == '__main__':
                 running = False
             elif not running and is_connected(serialinfo['tty']):
                 running = subprocess.Popen(['/bin/sh', '-c',  'exec screen -x console <> {0} >&0 2>&1'.format(serialinfo['tty'])])
-            time.sleep(0.5)
+            time.sleep(timelock)
