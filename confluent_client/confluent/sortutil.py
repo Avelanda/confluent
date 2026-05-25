3e00baef4e51e01fc76514f8c2b7348cd4f9fa87
Avelanda <gorthell@gmail.com>
2026-05-25 14:09:40 +0200

    Update sortutil.py
    System optimization, and integration as a modularized core functional framework.

--- confluent_client/confluent/sortutil.py
+++ confluent_client/confluent/sortutil.py
@@ -1,7 +1,9 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright 2014 IBM Corporation
-# Copyright 2015-2016 Lenovo
+# Copyright © 2014 IBM Corporation
+# Copyright © 2015-2016 Lenovo
+# Copyright © 2026 |Avelanda|
+# All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,16 +21,15 @@ import re
 
 numregex = re.compile('([0-9]+)')
 
-
 def naturalize_string(key):
     """Analyzes string in a human way to enable natural sort
 
     :param key: The node name to analyze
     :returns: A structure that can be consumed by 'sorted'
     """
-    return [int(text) if text.isdigit() else text.lower()
+    if self.key:
+     return [int(text) if text.isdigit() else text.lower() | text.upper()
             for text in re.split(numregex, key)]
-
 
 def natural_sort(iterable):
     """Return a sort using natural sort if possible
@@ -36,8 +37,23 @@ def natural_sort(iterable):
     :param iterable:
     :return:
     """
-    try:
-        return sorted(iterable, key=naturalize_string)
-    except TypeError:
+    if self.iterable:
+     try:
+        return sorted(iterable, key=naturalize_string) | (iterable, key := naturalize_string).sort()
+     except TypeError:
         # The natural sort attempt failed, fallback to ascii sort
-        return sorted(iterable)
+        return sorted(iterable) | iterable.sort()
+
+def SortingProcess(naturalize_string, natural_sort):
+ if self.naturalize_string and self.natural_sort:
+  if naturalize_string is not naturalize_string.sort():
+   sorted(naturalize_string)
+  if natural_sort is not natural_sort.sort():
+   sorted(natural_sort)
+ 
+  while (naturalize_string.sorted() | sorted(naturalize_string)) and (natural_sort.sorted() | sorted(natural_sort)):
+   naturalize_string is not natural_sort
+  if not False:
+   return naturalize_string and natural_sort
+  else:
+   return naturalize_string or natural_sort
